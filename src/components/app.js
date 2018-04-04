3c33a78888c9498728e797597d2a4079451bc2a9
Christian Brotherson <christian.brotherson@gmail.com>
2018-04-04 16:18:50 -0600

    added styles to header

--- src/components/app.js
+++ src/components/app.js
@@ -5,6 +5,13 @@ export default class App extends Component {
   render() {
     return (
       <div>
+        <header>
+          <h1>Birthday Countdown</h1>
+          <div className='header-skew'>
+            <div className='header-subskew'>
+            </div>
+          </div>
+        </header>
         <BirthdayForm />
       </div>
     );
